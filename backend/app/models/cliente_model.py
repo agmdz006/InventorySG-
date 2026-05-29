f7dda899886dfa7ebbaf26201da91790cbefa530
AGMDZ006 <alejandrogironmenendez@gmail.com>
2026-05-29 16:42:55 -0600

    modelo del cliente donde se parsearon los datos de las tablas SQL

---
+++ backend/app/models/cliente_model.py
@@ -0,0 +1,16 @@
+from app import db 
+from datatime import datetime
+
+class Cliente(db.Model):
+    __tablename__ = 'clientes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(150), nullable=False)
+    identificacion = db.Column(db.String(20), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    correo = db.Column(db.String(150), nullable=False)
+    fecha_registro = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Cliente {self.nombre} - {self.telefono}>'
+
